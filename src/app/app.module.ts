dcca6b317c1b6f76eae2e17fc12d77fde6d8d526
Konstantin <zkkorion@gmail.com>
2023-04-24 10:24:35 +0500

    add html css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {ButtonModule} from 'primeng/button';
+import {AppRoutingModule} from './components/app-routing.module';
+import {AuthComponent} from './components/auth/auth.component';
+import {MainComponent} from './components/main/main.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {TooltipModule} from 'primeng/tooltip';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AuthComponent,
+        MainComponent,
+    ],
+    imports: [
+        BrowserModule,
+        ButtonModule,
+        AppRoutingModule,
+        InputTextModule,
+        TooltipModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthComponent} from './auth/auth.component';
+import {MainComponent} from './main/main.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: 'auth', pathMatch: 'full'},
+    {path: 'auth', component: AuthComponent},
+    {path: 'workspace', component: MainComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
