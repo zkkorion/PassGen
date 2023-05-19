a8c63ef913361d245505fdea96e0a1c10cf1f45b
Zolotarev Konstantin <KZolotarev@htsts.ru>
2023-05-19 18:02:59 +0500

    add some css

--- src/app/components/app-routing.module.ts
+++ src/app/components/app-routing.module.ts
@@ -1,17 +1,19 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
-import {AuthComponent} from './auth/auth.component';
-import {MainComponent} from './main/main.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'auth', pathMatch: 'full'},
-    {path: 'auth', component: AuthComponent},
-    {path: 'workspace', component: MainComponent}
+    { path: '', redirectTo: 'auth', pathMatch: 'full' },
+    { path: 'auth', component: AuthComponent },
+    { path: 'workspace', component: MainComponent }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+
+}
