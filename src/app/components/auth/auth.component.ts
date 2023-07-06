e40e2000669ac70818cd4b8e14ae5465d6b7595b
Zolotarev Konstantin <KZolotarev@htsts.ru>
2023-07-06 20:03:03 +0500

    add some html css

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent
+{
 
 }
